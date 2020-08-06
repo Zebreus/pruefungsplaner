ea26c1b3cb4b8f4b847e61e6ffbd5dae7624610e
Lennart Eichhorn <lennart@madmanfred.com>
2020-08-06 12:33:19 +0200

    Fixed qml path in main

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     //qmlRegisterType<PruefungsplanerBackend>("examples.backend", 1, 0, "BackEnd");
     qmlRegisterSingletonType<PruefungsplanerManager>("org.pruefungsplaner.Backend", 1, 0, "Backend", &PruefungsplanerManager::getQmlInstance);
     QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
