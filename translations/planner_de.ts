066568a588d9da8a107ac4b063a46cfc505400d4
Lennart Eichhorn <lennart@madmanfred.com>
2020-09-05 15:40:38 +0200

    Added german localization

--- translations/planner_de.ts
+++ translations/planner_de.ts
@@ -8,14 +8,15 @@
         <source>Gruppe hinzufügen</source>
         <comment>popup heading</comment>
         <extracomment>Heading of popup to select groups</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Zug hinzufügen</translation>
     </message>
     <message>
         <location filename="../res/qml/AddGroupPopupForm.ui.qml" line="64"/>
         <source> </source>
         <comment>group filter placeholder</comment>
         <extracomment>Placeholder for group filter field</extracomment>
-        <translation type="unfinished"></translation>
+        <translatorcomment>Kein platzhalter</translatorcomment>
+        <translation> </translation>
     </message>
 </context>
 <context>
@@ -24,37 +25,37 @@
         <location filename="../src/connectionmanager.cpp" line="52"/>
         <source>Der Authentifizierungsserver ist gerade nicht erreichbar. Versuch es später nochmal.</source>
         <extracomment>Errormessage: auth server unreachable</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Der Authentifizierungsserver ist gerade nicht erreichbar. Versuch es später nochmal.</translation>
     </message>
     <message>
         <location filename="../src/connectionmanager.cpp" line="56"/>
         <source>Der Authentifizierungsserver hat ein Problem. Am besten schaust du mal nach ihm.</source>
         <extracomment>Errormessage: auth server error</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Der Authentifizierungsserver hat ein Problem. Am besten schaust du mal nach ihm.</translation>
     </message>
     <message>
         <location filename="../src/connectionmanager.cpp" line="60"/>
         <source>Der Authentifizierungsserver hat mit einer ungültige Nachricht geantwortet. Am besten schaust du mal nach ihm.</source>
         <extracomment>Errormessage: auth server invalid response</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Der Authentifizierungsserver hat mit einer ungültige Nachricht geantwortet. Am besten schaust du mal nach ihm.</translation>
     </message>
     <message>
         <location filename="../src/connectionmanager.cpp" line="64"/>
         <source>Der Authentifizierungsserver sendet seltsame Nachrichten. Am besten schaust du mal nach ihm.</source>
         <extracomment>Errormessage: auth server unexpected message</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Der Authentifizierungsserver sendet seltsame Nachrichten. Am besten schaust du mal nach ihm.</translation>
     </message>
     <message>
         <location filename="../src/connectionmanager.cpp" line="81"/>
         <source>Der Nutzername und das Password scheinen nicht zu passen. Bitte überprüfe deine Angaben und versuche es nochmal.</source>
         <extracomment>Errormessage: invalid username or password</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Der Nutzername und das Password scheinen nicht zu passen. Bitte überprüfe deine Angaben und versuche es nochmal.</translation>
     </message>
     <message>
         <location filename="../src/connectionmanager.cpp" line="88"/>
         <source>Der Pruefungsplanerserver ist gerade nicht erreichbar. Versuch es nachher nochmal.</source>
         <extracomment>Errormessage: backend not available</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Die Datenbank ist gerade nicht erreichbar. Versuch es nachher nochmal.</translation>
     </message>
 </context>
 <context>
@@ -63,27 +64,27 @@
         <location filename="../res/qml/ExamListItemForm.ui.qml" line="74"/>
         <source>Gruppen</source>
         <extracomment>Heading for list of groups</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Züge</translation>
     </message>
     <message>
         <location filename="../res/qml/ExamListItemForm.ui.qml" line="102"/>
         <source>Gruppe hinzufügen</source>
         <comment>add group</comment>
         <extracomment>Text for button to manage groups</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Züge verwalten</translation>
     </message>
     <message>
         <location filename="../res/qml/ExamListItemForm.ui.qml" line="117"/>
         <source>Bedingungen</source>
         <extracomment>Heading for list of constraints</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Bedingungen</translation>
     </message>
     <message>
         <location filename="../res/qml/ExamListItemForm.ui.qml" line="144"/>
         <source>Bedingung hinzufügen</source>
         <comment>add constraint</comment>
         <extracomment>Text for button to manage constraints</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Bedingungen verwalten</translation>
     </message>
 </context>
 <context>
@@ -92,39 +93,39 @@
         <location filename="../res/qml/LoginPopupForm.ui.qml" line="106"/>
         <source>Wilkommen beim Prüfungsplaner</source>
         <extracomment>Greeting in login popup</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Wilkommen beim Prüfungsplaner</translation>
     </message>
     <message>
         <location filename="../res/qml/LoginPopupForm.ui.qml" line="139"/>
         <source>Nutzername</source>
         <extracomment>Heading for username input</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Nutzername</translation>
     </message>
     <message>
         <location filename="../res/qml/LoginPopupForm.ui.qml" line="149"/>
         <source> </source>
         <comment>username placeholder</comment>
         <extracomment>Placeholder for username input</extracomment>
-        <translation type="unfinished"></translation>
+        <translation> </translation>
     </message>
     <message>
         <location filename="../res/qml/LoginPopupForm.ui.qml" line="165"/>
         <source>Passwort</source>
         <extracomment>Heading for password input</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Passwort</translation>
     </message>
     <message>
         <location filename="../res/qml/LoginPopupForm.ui.qml" line="175"/>
         <source> </source>
         <comment>password placeholder</comment>
         <extracomment>Placeholder for password input</extracomment>
-        <translation type="unfinished"></translation>
+        <translation> </translation>
     </message>
     <message>
         <location filename="../res/qml/LoginPopupForm.ui.qml" line="184"/>
         <source>Login</source>
         <extracomment>Login button text</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Anmelden</translation>
     </message>
 </context>
 <context>
@@ -133,13 +134,13 @@
         <location filename="../res/qml/PlanningView.qml" line="13"/>
         <source>Gruppe</source>
         <extracomment>New group default name</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Neuer Zug</translation>
     </message>
     <message>
         <location filename="../res/qml/PlanningView.qml" line="24"/>
         <source>Bedingung</source>
         <extracomment>New constraint default name</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Neue Bedingung</translation>
     </message>
 </context>
 <context>
@@ -148,27 +149,27 @@
         <location filename="../res/qml/PlanningViewForm.ui.qml" line="39"/>
         <source>Gruppen</source>
         <extracomment>Heading of groups section</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Züge</translation>
     </message>
     <message>
         <location filename="../res/qml/PlanningViewForm.ui.qml" line="102"/>
         <source>Neue Gruppe</source>
         <comment>create and add</comment>
         <extracomment>Button to add new group</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Zug erstellen</translation>
     </message>
     <message>
         <location filename="../res/qml/PlanningViewForm.ui.qml" line="131"/>
         <source>Bedingungen</source>
         <extracomment>Heading of constraints section</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Bedingungen</translation>
     </message>
     <message>
         <location filename="../res/qml/PlanningViewForm.ui.qml" line="195"/>
         <source>Neue Bedingung</source>
         <comment>create and add</comment>
         <extracomment>Button to add new constraint</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Bedingung erstellen</translation>
     </message>
 </context>
 <context>
@@ -178,7 +179,7 @@
         <source>Verschieben</source>
         <comment>move action</comment>
         <extracomment>Button to move item</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Verschieben</translation>
     </message>
 </context>
 <context>
@@ -187,7 +188,7 @@
         <location filename="../res/qml/TitleBarForm.ui.qml" line="45"/>
         <source>Prüfungsplaner</source>
         <extracomment>Application name</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Prüfungsplaner</translation>
     </message>
 </context>
 <context>
@@ -196,7 +197,7 @@
         <location filename="../res/qml/main.qml" line="13"/>
         <source>Prüfungsplaner</source>
         <extracomment>Title of application window</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Prüfungsplaner</translation>
     </message>
 </context>
 </TS>
