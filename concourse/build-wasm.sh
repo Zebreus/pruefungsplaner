c5edfeb756c38956515df8ce81d9d81c37a72590
Lennart Eichhorn <lennart@madmanfred.com>
2020-11-26 13:19:21 +0100

    Removed concourse build wasm task

--- concourse/build-wasm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-DESTDIR=../built-frontend
-qmake pruefungsplaner.pro QMAKE_CXXFLAGS_RELEASE="'"-O${optimization_level}"'" QMAKE_LFLAGS_RELEASE="'"-O${optimization_level}"'" DESTDIR=${DESTDIR}
-make -j ${make_threads}
-cp -r concourse/web/. ${DESTDIR}
